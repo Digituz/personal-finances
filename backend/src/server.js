b4522ca5a2b7fa3205c7badca8bfbd3aaf879687
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-24 09:31:29 -0300

    fixing indentation

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,23 +9,23 @@ app.use(morgan('tiny'));
 app.use(bodyParser.json());
 
 const jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: "https://digituz-corp.auth0.com/.well-known/jwks.json"
-    }),
-    audience: 'https://personal-finances.digituz.com.br',
-    issuer: "https://digituz-corp.auth0.com/",
-    algorithms: ['RS256']
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://digituz-corp.auth0.com/.well-known/jwks.json"
+  }),
+  audience: 'https://personal-finances.digituz.com.br',
+  issuer: "https://digituz-corp.auth0.com/",
+  algorithms: ['RS256']
 });
 
 app.get('/public', (req, res) => {
-    res.send({ message: 'public resource' });
+  res.send({message: 'public resource'});
 });
 
 app.get('/authorized', jwtCheck, (req, res) => {
-    res.send({ message: 'private resource' });
+  res.send({message: 'private resource'});
 });
 
 app.listen(process.env.PORT || 3000);
