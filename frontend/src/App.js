919cde9d29d5fb5b67d24b3c7204b1f5c170a567
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-24 22:54:55 -0300

    improving layout and installing/starting to configuring Auth0;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,37 @@
+import * as Auth0 from 'auth0-web';
 import React, {Component} from 'react';
 import Header from '@digituz/react-header';
 import Button from '@digituz/react-button';
 import './App.css';
 
 class App extends Component {
-  heyThere() {
-    alert('hey there!');
+  constructor(props) {
+    super(props);
+    Auth0.configure({
+      domain: 'digituz-corp.auth0.com',
+      audience: 'https://personal-finances.digituz.com.br',
+      clientID: 'wtgLEhG40Ns5v1HtHlZ3ZkKlci2McuUa',
+      redirectUri: 'http://localhost:3000/callback',
+      responseType: 'token id_token',
+      scope: 'openid profile manage:finances'
+    });
   }
+
+  signIn = Auth0.signIn;
+  signOut = Auth0.signOut;
 
   render() {
     return (
-      <div>
-        <Header title="Personal Finances"/>
-        <Button onClick={this.heyThere} text="Hey there!" />
+      <div className="digituz-panel">
+        <div className="digituz-panel-coloured-border"></div>
+        <div className="digituz-panel-header">
+          <Header title="Personal Finances">
+            <Button onClick={this.signIn} text="Sign In" />
+          </Header>
+        </div>
+        <div className="digituz-panel-body">
+
+        </div>
       </div>
     );
   }
