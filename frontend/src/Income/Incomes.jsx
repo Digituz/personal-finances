51146e5d03184060b61432623053c5bdfcc27816
Bruno Krebs <bruno.krebs@auth0.com>
2018-03-27 23:08:46 -0300

    using Dropdown component correctly and enabling users to delete incomes;

--- frontend/src/Income/Incomes.jsx
+++ frontend/src/Income/Incomes.jsx
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
-import {Card, Button, Dropdown, Table} from '@digituz/react-components';
+import {Card, Button, Dropdown, NotificationManager, Table} from '@digituz/react-components';
 import {maskCurrency} from 'mask-js';
 import * as PersonalFinances from '../Services/PersonalFinances';
 
 class Incomes extends Component {
+  constructor() {
+    super();
+    this.editIncome = this.editIncome.bind(this);
+    this.state = {
+      data: [],
+    }
+  }
+
   editIncome(income) {
     this.props.history.push(`/incomes/${income._id}`);
   }
@@ -13,9 +21,42 @@ class Incomes extends Component {
     this.props.history.push('/income');
   }
 
+  deleteIncome(income) {
+    PersonalFinances.remove(income._id)
+      .then(() => {
+        this.props.history.push('/incomes');
+        NotificationManager.success('Income removed successfully.');
+        this.loadIncomes();
+      })
+      .catch((err) => {
+        if (err.message && typeof err.message === 'string') return NotificationManager.danger(err.message);
+        NotificationManager.danger('Something went wrong.');
+      });
+  }
+
+  componentDidMount() {
+    this.loadIncomes();
+  }
+
+  loadIncomes() {
+    const data = PersonalFinances.get();
+    this.setState({
+      data,
+    });
+  }
+
   render() {
     const valueRenderer = (income) => (<span>{maskCurrency(income.value)}</span>);
-    const actionRenderer = (income) => (<Dropdown />);
+    const actionRenderer = (income) => {
+      const dropdownOptions = [
+        { label: 'Edit', default: true, onClick: () => { this.editIncome(income) }},
+        { label: 'Make a Copy', onClick: () => { this.editIncome(income) }},
+        { label: 'Delete', onClick: () => { this.deleteIncome(income) }},
+      ];
+      return (
+        <Dropdown options={dropdownOptions} />
+      );
+    };
 
     const columns = [
       { title: 'Description', property: 'description' },
@@ -25,7 +66,7 @@ class Incomes extends Component {
     return (
       <Card className="sm-12 md-10 md-pad-1 lg-8 lg-pad-2" title="Incomes">
         <Button onClick={() => { this.newIncome() }} text="New Income" />
-        <Table data={PersonalFinances.get()} columns={columns} />
+        <Table data={this.state.data} columns={columns} />
       </Card>
     );
   }
